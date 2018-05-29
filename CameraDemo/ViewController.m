ca0a4d591319c4503314ec7a9312b3aa69b187ff
jingjinzhou <jingjinzhou@tencent.com>
2018-05-29 14:26:39 +0800

    preview done

--- CameraDemo/ViewController.m
+++ CameraDemo/ViewController.m
@@ -7,16 +7,46 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
+@property (nonatomic, strong) AVCaptureSession *session;
+@property (nonatomic, strong) AVCaptureDevice *device;
+@property (nonatomic, strong) AVCaptureInput *input;
+@property (nonatomic, strong) AVCaptureOutput *output;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupSession];
+    [self runSession];
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)setupSession;
+{
+    self.session = [[AVCaptureSession alloc] init];
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error;
+    self.input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:&error];
+    if (self.input) {
+        [self.session addInput:self.input];
+    } else {
+        NSLog(@"!error!");
+    }
+    self.previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    self.previewLayer.frame = [self.view bounds];
+    [self.view.layer addSublayer:self.previewLayer];
+    
+}
+
+- (void)runSession;
+{
+    [self.session startRunning];
 }
 
 
